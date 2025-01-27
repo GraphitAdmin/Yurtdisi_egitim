2abf9100dc00d50d9d3e1eca1fdee70551f3babf
bozhovskiy <bozhovskiy18@gmail.com>
2025-01-27 14:36:34 +0200

    Update

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,9 @@ const nextConfig: NextConfig = {
             },
         ],
     },
+    env: {
+        NEXT_GOOGLE_API_KEY: process.env.NEXT_GOOGLE_API_KEY,
+    }
 };
 
 export default nextConfig;
