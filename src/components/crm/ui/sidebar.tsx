841b015093642df98c90afc28e511b9395cc4b52
bozhovskiy <bozhovskiy18@gmail.com>
2025-01-30 14:35:56 +0200

    Update

--- src/components/crm/ui/sidebar.tsx
+++ src/components/crm/ui/sidebar.tsx
@@ -1,15 +1,14 @@
 import Link from "next/link"
-import {Users, School, FileText, Calendar, Settings, Building,NotebookPen} from 'lucide-react'
-import Logo from "@/components/UI/Logo/Logo";
+import {Users, School, Calendar, Settings, Building,NotebookPen} from 'lucide-react'
 import React from "react";
 
 const navigationItems = [
-    { name: "Students", icon: Users, href: "/students" },
+    { name: "Students PDF requests", icon: Users, href: "/students" },
+    { name: "Students from Forms", icon: Users, href: "/students" },
     { name: "Cities", icon: Building, href: "/city" },
     { name: "Schools", icon: School, href: "/school" },
     { name: "Blogs", icon: NotebookPen, href: "/blog" },
     { name: "Events", icon: Calendar, href: "/event" },
-    { name: "Sitemap", icon: FileText, href: "/sitemap" },
     { name: "General Settings", icon: Settings, href: "/settings" },
 ]
 
