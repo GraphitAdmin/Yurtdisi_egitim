fdd0766247b371bffe50e0c1e78be9b11c92cd34
bozhovskiy <bozhovskiy18@gmail.com>
2025-01-30 10:29:58 +0200

    Dropdown update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import "./layout.css"
 import React from "react";
 import { Toaster } from 'react-hot-toast';
 import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import { Analytics } from "@vercel/analytics/react"
 const urbanist = Urbanist({
     weight: ['200','300','400', '500', '600', '700', '800'],
     subsets: ["latin"],
@@ -29,6 +29,7 @@ export default function RootLayout({
         <html lang="en" className={`${urbanist.variable}`}>
         <body>
         <div>
+            <Analytics/>
             <SpeedInsights/>
             {children}
             <Toaster/>
