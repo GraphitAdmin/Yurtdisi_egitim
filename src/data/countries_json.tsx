28c665a3c33e2926c6b51d4ff6eeafda39c20a3c
bozhovskiy <bozhovskiy18@gmail.com>
2025-01-31 12:17:58 +0200

    Update

--- src/data/countries_json.tsx
+++ src/data/countries_json.tsx
@@ -1,13 +1,13 @@
-import UKImage from "@/assets/countries/UK.webp";  // Image for the UK
-import USImage from "@/assets/countries/US.webp";  // Image for the US
-import MaltaImage from "@/assets/countries/Malta.webp";  // Image for Malta
-import GermanyImage from "@/assets/countries/Germany.webp";  // Image for Germany
-import SwitzerlandImage from "@/assets/countries/Switzerland.webp";  // Image for Switzerland
-import SpainImage from "@/assets/countries/Spain.webp";  // Image for Spain
-import FranceImage from "@/assets/countries/France.webp";  // Image for France
-import ItalyImage from "@/assets/countries/Italy.webp";  // Image for Italy
-import PortugalImage from "@/assets/countries/Portugal.webp";  // Image for Portugal
-import AustriaImage from "@/assets/countries/Austria.webp";  // Image for Austria
+import UKImage from "@/assets/countries/UK.webp";
+import USImage from "@/assets/countries/US.webp";
+import MaltaImage from "@/assets/countries/Malta.webp";
+import GermanyImage from "@/assets/countries/Germany.webp";
+import SwitzerlandImage from "@/assets/countries/Switzerland.webp";
+import SpainImage from "@/assets/countries/Spain.webp";
+import FranceImage from "@/assets/countries/France.webp";
+import ItalyImage from "@/assets/countries/Italy.webp";
+import PortugalImage from "@/assets/countries/Portugal.webp";
+import AustriaImage from "@/assets/countries/Austria.webp";
 
 export const UK = {
     name: 'United Kingdom',
@@ -16,7 +16,7 @@ export const UK = {
     imgPost: UKImage,
     language: 'English',
     population: '54 million (approx.)',
-    faceMeasurement: '243610',  // Land area in km²
+    faceMeasurement: '243610',  
     currency: 'Pound Sterling (GBP)',
     telephoneCode: '+44'
 };
@@ -28,7 +28,7 @@ export const US = {
     imgPost: USImage,
     language: 'English',
     population: '331 million (approx.)',
-    faceMeasurement: '9833517',  // Land area in km²
+    faceMeasurement: '9833517',  
     currency: 'United States Dollar (USD)',
     telephoneCode: '+1'
 };
@@ -40,7 +40,7 @@ export const Malta = {
     imgPost: MaltaImage,
     language: 'Maltese, English',
     population: '514,000 (approx.)',
-    faceMeasurement: '316',  // Land area in km²
+    faceMeasurement: '316',  
     currency: 'Euro (EUR)',
     telephoneCode: '+356'
 };
@@ -52,7 +52,7 @@ export const Germany = {
     imgPost: GermanyImage,
     language: 'German',
     population: '83 million (approx.)',
-    faceMeasurement: '357022',  // Land area in km²
+    faceMeasurement: '357022',  
     currency: 'Euro (EUR)',
     telephoneCode: '+49'
 };
@@ -62,9 +62,9 @@ export const Switzerland = {
     link: '/switzerland',
     capital: 'Bern',
     imgPost: SwitzerlandImage,
-    language: 'German, French, Italian, Romansh',
+    language: 'German, French, Italian',
     population: '8.6 million (approx.)',
-    faceMeasurement: '41285',  // Land area in km²
+    faceMeasurement: '41285',
     currency: 'Swiss Franc (CHF)',
     telephoneCode: '+41'
 };
@@ -76,7 +76,7 @@ export const Spain = {
     imgPost: SpainImage,
     language: 'Spanish',
     population: '47 million (approx.)',
-    faceMeasurement: '505992',  // Land area in km²
+    faceMeasurement: '505992',  
     currency: 'Euro (EUR)',
     telephoneCode: '+34'
 };
@@ -88,7 +88,7 @@ export const France = {
     imgPost: FranceImage,
     language: 'French',
     population: '67 million (approx.)',
-    faceMeasurement: '643801',  // Land area in km²
+    faceMeasurement: '643801',  
     currency: 'Euro (EUR)',
     telephoneCode: '+33'
 };
@@ -100,7 +100,7 @@ export const Italy = {
     imgPost: ItalyImage,
     language: 'Italian',
     population: '60 million (approx.)',
-    faceMeasurement: '301340',  // Land area in km²
+    faceMeasurement: '301340',  
     currency: 'Euro (EUR)',
     telephoneCode: '+39'
 };
@@ -112,7 +112,7 @@ export const Portugal = {
     imgPost: PortugalImage,
     language: 'Portuguese',
     population: '10 million (approx.)',
-    faceMeasurement: '92212',  // Land area in km²
+    faceMeasurement: '92212',  
     currency: 'Euro (EUR)',
     telephoneCode: '+351'
 };
@@ -124,7 +124,7 @@ export const Austria = {
     imgPost: AustriaImage,
     language: 'German',
     population: '9 million (approx.)',
-    faceMeasurement: '83871',  // Land area in km²
+    faceMeasurement: '83871',  
     currency: 'Euro (EUR)',
     telephoneCode: '+43'
 };
