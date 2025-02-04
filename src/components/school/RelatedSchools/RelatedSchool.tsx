050618ccb519d479f5d70f1bbe877b4903d8ad41
bozhovskiy <bozhovskiy18@gmail.com>
2025-02-04 16:14:21 +0200

    Test

--- src/components/school/RelatedSchools/RelatedSchool.tsx
+++ src/components/school/RelatedSchools/RelatedSchool.tsx
@@ -13,7 +13,7 @@ const RelatedSchool: React.FC<IRelated> = ({imgPost,title, link, description}) =
                 width='273' height={152}
                 className="w-full" src={blobUrl+imgPost} alt={title}/>
             <h5 style={{textAlign: 'left',marginTop:12}}>{title}</h5>
-            <p style={{textAlign: 'left'}}>{description}</p>
+            <p style={{textAlign: 'left',}}>{description}</p>
             <Button label='School details' btnStyle={{width:'100%',padding:'12px 0',marginTop:16}}/>
         </Link>
     )
