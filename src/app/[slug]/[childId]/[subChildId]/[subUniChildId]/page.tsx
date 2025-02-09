7472c3ab76be1a3dae46f1de188368ad33182949
bozhovskiy <bozhovskiy18@gmail.com>
2025-02-09 18:10:06 +0200

    Test

--- src/app/[slug]/[childId]/[subChildId]/[subUniChildId]/page.tsx
+++ src/app/[slug]/[childId]/[subChildId]/[subUniChildId]/page.tsx
@@ -2,11 +2,13 @@ import Navbar from "@/components/UI/Navbar/Navbar"
 import Tabs from "@/components/UI/Tabs/Tabs"
 import Subscribe from "@/components/UI/FAQ/Subscribe"
 import Footer from "@/components/UI/Footer/Footer"
-import type { ISchool } from "@/utils/interfaces"
-import { blobUrl, cleanTitle } from "@/utils/utils"
-import { notFound } from "next/navigation"
+import type {ISchool} from "@/utils/interfaces"
+import {blobUrl, cleanTitle} from "@/utils/utils"
+import {notFound} from "next/navigation"
 import SchoolPageContent from "@/components/schoolPageContent/schoolPageContent"
-import type { Metadata } from "next"
+import type {Metadata} from "next"
+
+type paramsType = Promise<{ slug: string, childId: string, subChildId: string, subUniChildId: string }>;
 
 type Params = {
     slug: string
@@ -43,7 +45,7 @@ const fetchSchool = async (slug: string, childId: string, subUniChildId: string,
         for (const school of jsonData) {
             const cleanedTitle = cleanTitle(school.title)
             if (cleanedTitle === cleanedName) {
-                return { school, relatedSchools, moreSchools }
+                return {school, relatedSchools, moreSchools}
             }
         }
     } catch (err) {
@@ -52,25 +54,28 @@ const fetchSchool = async (slug: string, childId: string, subUniChildId: string,
     }
 }
 
-export async function generateMetadata({ params }: { params: Params }): Promise<Metadata> {
-    const { slug, childId, subUniChildId, subChildId } =await params
-    console.log(slug, childId, subUniChildId, subChildId)
+export async function generateMetadata({params}: { params: paramsType }): Promise<Metadata> {
+    const {slug, childId, subUniChildId, subChildId} = await params
     // const data = await fetchSchool(slug, childId, subUniChildId, subChildId)
 
     const title =
         subUniChildId.replace(/-/g, " ").replace(/\b\w/g, (char) => char.toUpperCase()) +
         " - " +
         childId.replace(/-/g, " ").replace(/\b\w/g, (char) => char.toUpperCase())
-
+    console.log(slug, childId, subUniChildId, subChildId)
     // const description = data?.school?.school_overview
     return {
         title,
-        description:'You can find everything you want to know before you start researching language education abroad here.',
+        description: ''
     }
 }
 
-export default async function Home({ params }: { params: Params }) {
-    const { slug, childId, subUniChildId, subChildId } = params
+export default async function Home({
+                                       params,
+                                   }: {
+    params: paramsType;
+}) {
+    const {slug, childId, subUniChildId, subChildId} = await params
     const data = await fetchSchool(slug, childId, subUniChildId, subChildId)
 
     if (!data || !data.school) {
@@ -79,16 +84,16 @@ export default async function Home({ params }: { params: Params }) {
 
     return (
         <div>
-            <Navbar home={false} />
-            <Tabs />
+            <Navbar home={false}/>
+            <Tabs/>
             <SchoolPageContent
                 subUniChildId={subUniChildId}
                 school={data.school}
                 relatedSchools={data.relatedSchools}
                 moreSchools={data.moreSchools}
             />
-            <Subscribe />
-            <Footer />
+            <Subscribe/>
+            <Footer/>
         </div>
     )
 }
